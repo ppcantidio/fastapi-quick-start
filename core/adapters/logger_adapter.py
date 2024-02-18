6583e4c79e39ccf9423d4ef7c9ff89348076202d
Pedro Cantidio <ppcantidio@gmail.com>
2024-02-18 00:52:10 -0300

    refact: remiving desnecessary files

--- core/adapters/logger_adapter.py
+++
@@ -1,8 +0,0 @@
-import structlog
-
-
-class LoggerAdapter:
-    @classmethod
-    def get_logger(cls, logger_name: str) -> structlog.BoundLogger:
-        logger = structlog.stdlib.get_logger(logger_name)
-        return logger
